b13642435a2b87357e954b1f15c15ad16e696d02
tigerding <zhiyuanding01@gmail.com>
2025-12-09 23:49:28 -0500

    refactor

---
+++ robots/__init__.py
@@ -0,0 +1,4 @@
+from .scrape_announcements import scrape_announcements_page
+from .scrape_assignments import scrape_assignments_page
+
+__all__ = ["scrape_announcements_page", "scrape_assignments_page"]
